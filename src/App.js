ab9c7ad45eeda22c0d11ea48093fab7eb5d21492
mnezmah <mario.nezmah@gmail.com>
2019-03-29 18:47:32 +0100

    added axios dependency

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Color from './components/Color';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+state = {
+  colors:  []
+};
+
+
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <Color />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
--- src/components/Color.js
+++ src/components/Color.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
  class Color extends Component {
+   constructor(props){
+     super(props);
+       this.state = {
+         color: 'black'
+       }
+     }
+
   render() {
     return (
       <div>
         <h1>Hello from color.js !</h1>
+       
       </div>
     )
   }
